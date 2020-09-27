4f24682c762cb65c7cdeac955cf645f413b8c677
Shrfu31 <Shrfu31@ChrisBlkPro2013.localdomain>
2020-09-27 16:06:58 -0400

    Added an arrow function to pass parameters to event handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,20 +7,53 @@ class Counter extends Component {
     imageUrl: "https://picsum.photos/200",
   };
 
+  // //solution to bind event handlers to this
+  // constructor() {
+  //   //you need this if you are using the constructor in your class in react because no matter what you build, it will be a child component to the overall inheritance hiearchy.
+  //   super();
+  //   //binding this to handleIncrememnt and then assigning it.
+  //   //You have to do this for every event handler so this way is a bit noisy.
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  //   console.log("Constructor", this);
+  // }
+
   // styles = {
   //   fontSize: 10,
   //   fontWeight: "bold",
+  // };
+  renderTags() {
+    if (this.state.tags.length === 0) return <p> There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map((bask) => (
+          <li key={bask}> {bask} </li>
+        ))}
+      </ul>
+    );
+  }
+  handleIncrement = (product) => {
+    //shows in console
+    console.log(product);
+    console.log("Increment Button Clicked!", this.state.count);
+    //changes state the official way so React is aware.
+    this.setState({ count: this.state.count + 1 });
+  };
+  // doHandleIncrement = () => {
+  //   this.handleIncrement({ id: 1 });
   // };
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment </button>
-        <ul>
-          {this.state.tags.map((bask) => (
-            <li key={bask}>{bask}</li>
-          ))}
-        </ul>
+        <button
+          onClick={() => this.handleIncrement()}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment{" "}
+        </button>
+        {this.state.tags.length === 0 && <p>Please create a new tag!</p>}
+
+        <ul>{this.renderTags()}</ul>
       </div>
     );
   }
